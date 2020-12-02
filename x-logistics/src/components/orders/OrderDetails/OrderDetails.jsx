95d6ca80792c49c919bd66f6b71e116e15e4b97c
Afonso Mendonca <afonsomendonca91@gmail.com>
2020-12-02 17:27:05 +0000

    shitty popup with row and column options for item storage

--- x-logistics/src/components/orders/OrderDetails/OrderDetails.jsx
+++ x-logistics/src/components/orders/OrderDetails/OrderDetails.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
 import styles from './OrderDetails.module.css';
 
@@ -29,14 +30,57 @@ const OrderDetails = (props) => {
                                     <button>Confirm</button>
                                 )}  
                                 modal="true"
-                                closeOnDocumentClick
                             >
-                                <span>Popup content</span>
+                                <OrderStoragePopup/>
                             </Popup>
                         </section>
                     )
                 })
             }
+        </div>
+    )
+}
+
+const OrderStoragePopup = (props) => {
+    const options = [
+        'A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7',
+        'B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7',
+        'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7',
+        'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7',
+        'E1', 'E2', 'E3', 'E4', 'E5', 'E6', 'E7',
+        'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7'
+    ];
+
+    const rowOptions = [
+        'A', 'B', 'C', 'D', 'E', 'F'
+    ];
+
+    const columnOptions = [
+        '1', '2', '3', '4', '5', '6', '7'
+    ];
+
+    return(
+        <div>
+            <span className={styles.storagePopup}>Choose storage section:</span>
+            <select>
+                {
+                    rowOptions.map((option) => {
+                        return(
+                            <option>{option}</option>
+                        )
+                    })
+                }
+            </select>
+            <select>
+                {
+                    columnOptions.map((option) => {
+                        return(
+                            <option>{option}</option>
+                        )
+                    })
+                }
+            </select>
+            <button>Confirm</button>
         </div>
     )
 }
