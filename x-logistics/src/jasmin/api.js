a967ff99aad6bba59bccbcdc133e9a18a5d258d0
Gaspar99 <pasgas99@hotmail.com>
2020-12-03 13:13:45 +0000

    Started jasmin communication

---
+++ x-logistics/src/jasmin/api.js
@@ -0,0 +1,7 @@
+const url = "https://my.jasminsoftware.com";
+const tenant = "242853";
+const organization = "242853-0001";
+
+const apiURL = url + "/api/" + tenant + "/" + organization + "/";
+
+export default apiURL;
