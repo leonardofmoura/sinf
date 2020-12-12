da59fa0df44f25f030c531761f5f7cffa953623d
Afonso Mendonca <afonsomendonca91@gmail.com>
2020-12-12 14:52:12 +0000

    fixed bug on pending storage. added confirm storage pop up as well

--- x-logistics/src/components/orders/PendingStorage/PendingStorage.jsx
+++ x-logistics/src/components/orders/PendingStorage/PendingStorage.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
 
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+
 import Tabel from '../../tabel/Tabel/Tabel';
 import TabelHeader from '../../tabel/TabelHeader/TabelHeader';
 import TabelRow from '../../tabel/TabelRow/TabelRow';
@@ -8,8 +11,10 @@ import TabelRowSubRow from '../../tabel/TabelRowSubRow/TabelRowSubRow';
 import { sendJasminRequest } from '../../../jasmin/request';
 import { getWarehouseItems } from '../../../jasmin/inventory';
 
+import styles from './PendingStorage.module.css';
+
 const PendingStorage = (props) => {
-    const tableHeaders = ['ID', 'Name', 'Category', 'Quantity'];
+    const tableHeaders = ['ID', 'Name', 'Category', 'Quantity', 'Storage'];
 
     const [items, setItems] = useState([]);
 
@@ -39,15 +44,92 @@ const PendingStorage = (props) => {
                     return(
                         <TabelRow
                             key={index}
-                            data={items}
-                        >
-
-                        </TabelRow>
+                            data={item}
+                            actionComponent={
+                                <Popup trigger={open => (<button>Confirm</button>)} modal='true'>
+                                    <ItemsStoragePopup/>
+                                </Popup>}
+                        />
                     )
                 })
             }
         </Tabel>
     )
 }
+
+const ItemsStoragePopup = (props) => {
+    const [selectedRow, setRow] = useState('A');
+    const [selectedCol, setCol] = useState('1');
+
+    const _handleRowChange = (event) => {
+        setRow(event.target.value);
+    }
+
+    const _handleColChange = (event) => {
+        setCol(event.target.value);
+    }
+
+    const _confirmItemStorage = async () => {
+        const body = {
+            loadingStreetName: 'Dr. Roberto Frias',
+            loadingBuildingNumber: '1',
+            loadingPostalZone: '4200-465',
+            loadingCityName: 'Porto',
+            loadingCountry: 'PT',
+            company: 'GXSA',
+            sourceWarehouse: 'RECEPTION',
+            targetWarehouse: selectedRow + selectedCol,
+            documentLines: [
+                {
+                    quantity: props.item.quantity,
+                    materialsItem: props.item.purchasesItem,
+                }
+            ],
+        };
+        console.log(props.item);
+        
+        const response = await sendJasminRequest(
+            `materialsManagement/stockTransferOrders`,
+            'POST',
+            body,
+            );
+
+        console.log('RESPONSE: ');
+        console.log(response);
+    }
+
+    const rowOptions = [
+        'A', 'B', 'C', 'D', 'E', 'F'
+    ];
+
+    const columnOptions = [
+        '1', '2', '3', '4', '5', '6', '7'
+    ];
+
+    return(
+        <div>
+            <span className={styles.storagePopup}>Choose storage section:</span>
+            <select value={selectedRow} onChange={_handleRowChange}>
+                {
+                    rowOptions.map((option) => {
+                        return(
+                            <option key={option}>{option}</option>
+                        )
+                    })
+                }
+            </select>
+            <select value={selectedCol} onChange={_handleColChange}>
+                {
+                    columnOptions.map((option) => {
+                        return(
+                            <option key={option}>{option}</option>
+                        )
+                    })
+                }
+            </select>
+            <button onClick = {_confirmItemStorage}>Confirm</button>
+        </div>
+    )
+};
 
 export default PendingStorage;
